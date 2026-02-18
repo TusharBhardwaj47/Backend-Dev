fd59cdec58bcd2044f375981105490e91dbc26ca
TusharBhardwaj47 <tusharbhardwaj635@gmail.com>
2026-02-18 10:58:09 +0530

    Payroll-project

--- custom-modules/app.js
+++
@@ -1,7 +0,0 @@
-
-const stringUtils = require("./stringUtils");
-const text = "hello world";
-
-console.log(stringUtils.capitalize(text)); 
-console.log(stringUtils.reverse(text));    
-console.log(stringUtils.countVowels(text)); 
